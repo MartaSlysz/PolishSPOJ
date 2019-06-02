1032dd57c28bbdd80818bd8e4a006580c1d8c1b1
Marta Slysz <martaslysz93@gmail.com>
2019-06-02 12:19:56 +0200

    added PTEST solution

---
+++ src/main/java/com/slyszmarta/algs/Algs/PTEST.java
@@ -0,0 +1,19 @@
+package com.slyszmarta.algs.Algs;
+
+import java.util.Scanner;
+
+public class PTEST {
+
+    public static void main(String[] args) {
+        
+        //do SPOJA należy wklejać bez SOUTów, goły algorytm
+
+//        System.out.println("Podaj liczbę A.");
+        Scanner inputScanner = new Scanner( System.in );
+        int a = inputScanner.nextInt();
+//        System.out.println("Podaj liczbę B.");
+        int b = inputScanner.nextInt();
+        int c = a + b;
+        System.out.println(c);
+    }
+}
